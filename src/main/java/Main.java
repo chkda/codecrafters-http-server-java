156d1326e6f51e56d9bc77179ebd31e34326bb4f
Chhaya <chayakudas.das56@gmail.com>
2025-04-24 21:45:05 +0530

    Adding header support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,15 +24,10 @@ public class Main {
             InputStream inputStream = clientSocket.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String requestLine = bufferedReader.readLine();
 
             OutputStream outputStream = clientSocket.getOutputStream();
 
-            if (requestLine == null || requestLine.isEmpty()) {
-                notFoundResponseHandler(outputStream);
-            } else {
-                responseHandler(requestLine, outputStream);
-            }
+            responseHandler(bufferedReader, outputStream);
 
             System.out.println("accepted new connection");
         } catch (IOException e) {
@@ -70,21 +66,50 @@ public class Main {
         writer.flush();
     }
 
-    static void responseHandler(String requestLine, OutputStream writer) throws IOException {
-        String[] parts = requestLine.split(" ");
-        if (parts.length >= 2) {
-            String method = parts[0];
-            String requestTarget = parts[1];
-            if (requestTarget == null) {
-                notFoundResponseHandler(writer);
-            } else if (requestTarget.equals("/")) {
-                successResponseHandler(writer);
-            } else if (requestTarget.startsWith("/echo/")) {
-                String body = requestTarget.replace("/echo/", "");
-                successResponseHandler(body, writer);
-            } else {
-                notFoundResponseHandler(writer);
+    static HashMap<String, String> extractHeaders(BufferedReader reader) throws IOException{
+        String headerline = reader.readLine();
+        HashMap<String, String> hashMap = new HashMap<>();
+        while (headerline != null && !headerline.isEmpty()) {
+            int ind = headerline.indexOf(":");
+            if (ind > 0) {
+                String headerKey = headerline.substring(0, ind).toLowerCase();
+                String headerValue = headerline.substring(ind+1).trim();
+                hashMap.put(headerKey, headerValue);
             }
+            headerline = reader.readLine();
         }
+        return hashMap;
+    }
+
+    static void responseHandler(BufferedReader reader, OutputStream writer) throws IOException {
+        String requestLine = reader.readLine();
+        if (requestLine == null || requestLine.isEmpty()) {
+            notFoundResponseHandler(writer);
+            return;
+        }
+
+        String[] parts = requestLine.split(" ");
+        if (parts.length < 2) {
+            notFoundResponseHandler(writer);
+            return;
+        }
+
+        String method = parts[0];
+        String requestTarget = parts[1];
+        HashMap<String, String> headers = extractHeaders(reader);
+        if (requestTarget == null) {
+            notFoundResponseHandler(writer);
+        } else if (requestTarget.equals("/")) {
+            successResponseHandler(writer);
+        } else if (requestTarget.startsWith("/echo/")) {
+            String endpoint = requestTarget.replace("/echo/", "");
+            successResponseHandler(endpoint, writer);
+        } else if (requestTarget.equals("/user-agent") && headers.containsKey("user-agent")) {
+            String header = headers.get("user-agent");
+            successResponseHandler(header, writer);
+        } else {
+            notFoundResponseHandler(writer);
+        }
+
     }
 }
